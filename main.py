e4f4a4bb1a248d6d3d097c2b036956d61a606627
yogesh <yogesh.bisht@miko.ai>
2023-08-27 23:53:06 +0530

    update API endpoints

--- main.py
+++ main.py
@@ -1,107 +1,138 @@
-from fastapi import Depends, FastAPI, HTTPException, Request, Form
+import json
+from fastapi import FastAPI, Request, Form, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
-import json
-from sqlalchemy.orm import Session
-from typing import List
-
-import crud, models, schemas
-from models import User, Item, ItemCreate, Order
+import schemas
+from models import User, Item, Order
 
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 
 templates = Jinja2Templates(directory="templates")
 
 
-# # Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+@app.middleware("http")
+async def validate_user(request: Request, call_next):
+    print(request.session)
+    response = await call_next(request)
+    return response
+
 
 @app.get("/register", response_class=HTMLResponse)
-def get_register_form(request:Request):
-    return templates.TemplateResponse("register.html", {"request": request, "text": "register"})
+def get_registration_page(request: Request):
+    return templates.TemplateResponse("register.html", {
+        "request": request,
+        "user_email": None,
+        "text": "register"
+    })
 
 
 @app.post("/register", response_model=schemas.User)
-def create_user(email: str = Form(...), password: str = Form(...)):
+def register_user(request: Request, email: str = Form(...), password: str = Form(...)):
     user = schemas.UserCreate(email=email, password=password)
     db_user = User.get_user_by_email(email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return User.create_user(user=user)
+    if not db_user:
+        User.create_user(user=user)
+    redirect_url = request.url_for('get_login_page')    
+    return RedirectResponse(redirect_url)
 
 
+@app.get("/", response_class=HTMLResponse)
 @app.get("/login", response_class=HTMLResponse)
-def login(request:Request):
-    return templates.TemplateResponse("register.html", {"request": request, "text": "login"})
+def get_login_page(request: Request):
+    return templates.TemplateResponse("register.html", {
+        "request": request,
+        "user_email": None,
+        "text": "login"
+    })
 
 
 @app.post("/login", response_model=dict)
-def login_user(request:Request, email: str = Form(...), password: str = Form(...)):
+def login_user(request: Request, email: str = Form(...), password: str = Form(...)):
     user = schemas.UserCreate(email=email, password=password)
-    is_used_verified = User.verify_user(user)
+    is_used_verified, user_id = User.verify_user(user)
     if not is_used_verified:
-        raise HTTPException(status_code=401, detail="Invalid email or password")
-    return templates.TemplateResponse("index.html", {"request": request, "mykart": Item.get_items()})
+        redirect_url = request.url_for('get_login_page')    
+        return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    
+    request.session["session_id"] = user_id, email
+    redirect_url = request.url_for('get_user_kart')    
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
 
-@app.get("/mykart/{user_id}", response_model=dict)
-def get_user_kart(request:Request, user_id: int):
+@app.get("/logout")
+def logout(request: Request):
+    if request.session.get("session_id"):
+        request.session.pop("session_id")
+    redirect_url = request.url_for('get_login_page')    
+    return RedirectResponse(redirect_url)
+
+
+@app.get("/mykart", response_model=dict)
+def get_user_kart(request: Request):
+    user_id, email = request.session.get("session_id", [None, None])
+    if not user_id:
+        redirect_url = request.url_for('get_login_page')    
+        return RedirectResponse(redirect_url)
     return templates.TemplateResponse("mykart.html", {
         "request": request,
-        "user_id": user_id, 
+        "user_email": email,
         "mykart": Order.get_order_for_customer(user_id)
     })
 
 
-@app.get("/add_to_kart/{user_id}", response_class=HTMLResponse)
-def add_item_to_kart(request:Request, user_id: int):
-    error = None
+@app.get("/add_to_kart", response_class=HTMLResponse)
+def add_item_to_kart(request: Request):
+    user_id, email = request.session.get("session_id", [None, None])
+    if not user_id:
+        redirect_url = request.url_for('get_login_page')    
+        return RedirectResponse(redirect_url)
     return templates.TemplateResponse("home.html", {
-        "request": request, 
-        "medicines": Item.get_items(),
-        "error": error
-    })  
+        "request": request,
+        "user_email": email,
+        "medicines": Item.get_items()
+    })
 
 
-@app.post("/add_to_kart/{user_id}", response_class=HTMLResponse)
-def add_item_to_kart(request:Request, user_id: int, medicine: str = Form(...), qty: int = Form(...)):
-# def add_item_to_kart(request:Request, user_id: int, qty: int = Form(...)):
-    item = schemas.OrderCreate(item_json=json.dumps({medicine: qty}), customer_id=user_id)
-    error = None
-    try: 
-        is_created = Order.create_order(item)
-    except:
-        error = "Item already exists" 
-    return templates.TemplateResponse("home.html", {"request": request, "error": error})   
+@app.post("/add_to_kart", response_class=HTMLResponse)
+def add_item_to_kart(request: Request, medicine: str = Form(...), qty: int = Form(...)):
+    user_id, email = request.session.get("session_id", [None, None])
+    if not user_id:
+        redirect_url = request.url_for('get_login_page')    
+        return RedirectResponse(redirect_url)
+    item = schemas.OrderCreate(
+        item_json=json.dumps({medicine: qty}), 
+        customer_id=user_id
+    )
+    is_created = Order.create_order(item)
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "user_email": email,
+        "medicines": Item.get_items()
+    })
 
 
 @app.get("/add_item", response_class=HTMLResponse)
-def add_item(request:Request, error=None): 
-    return templates.TemplateResponse("add_item.html", {"request": request, "error": None})   
+def add_item(request: Request):
+    return templates.TemplateResponse("add_item.html", {"request": request, "error": None})
 
 
 @app.post("/add_item", response_class=HTMLResponse)
-def add_item(request:Request, title: str = Form(...), qty: int = Form(...)):
+def add_item(request: Request, title: str = Form(...), qty: int = Form(...)):
     item = schemas.ItemCreate(title=title, qty=qty)
     error = None
-    try: 
+    try:
         is_created = Item.create_item(item=item)
     except:
-        error = "Item already exists" 
-    return templates.TemplateResponse("add_item.html", {"request": request, "error": error})   
+        error = "Item already exists"
+    return templates.TemplateResponse("add_item.html", {"request": request, "error": error})
 
 
+app.add_middleware(SessionMiddleware, secret_key="some-random-string")
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="127.0.0.1", port=5049)
-
