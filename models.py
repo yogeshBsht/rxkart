2c8d1b23cde7ffcf1064b53b4ff6a02cdc480b61
yogesh <yogesh.bisht@miko.ai>
2023-09-16 00:30:28 +0530

    Updated Item, Cart & Order models

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import json
 from database import Base, SessionLocal, engine
+from datetime import datetime
 from fastapi import Depends
-from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey, DateTime, Float
 from sqlalchemy.orm import Session, relationship
 
 from schemas import UserCreate, UserUpdate, ItemCreate, OrderCreate, CartCreate
@@ -78,7 +79,6 @@ class Item(Base):
 
     item_id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, unique=True)
-    # qty = Column(Integer, default=0)
     price = Column(Integer, default=0)
     carts = relationship("Cart", back_populates="items")
 
@@ -110,26 +110,20 @@ class Cart(Base):
     item_id = Column(Integer, ForeignKey('items.item_id'))
     qty = Column(Integer)
     customer_id = Column(Integer, ForeignKey('users.user_id'))
+    order_id = Column(Integer, ForeignKey('orders.order_id'), default=None)
+    orders = relationship("Order", back_populates="carts")
     items = relationship("Item", back_populates="carts")
     customer = relationship("User", back_populates="carts")
 
     def __str__(self):
         return f"OrderId-{self.id}, ItemId-{self.item_id}, CustomerId-{self.customer_id}"
 
-    # @classmethod
-    # def get_order_by_id(cls, order_id: int):
-    #     """Get ordered items for the order_id"""
-    #     db = SessionLocal()
-    #     order = db.query(cls).filter(cls.order_id==order_id).first()
-    #     db.close()
-    #     ordered_items = json.loads(order.ordered_items) if order else {}
-    #     return ordered_items
-
     @classmethod
     def get_cart_items_for_customer(cls, customer_id: int):
         """Get cart items for the customer_id"""
         db = SessionLocal()
-        cart = db.query(cls, Item).filter(cls.item_id==Item.item_id).filter(cls.customer_id==customer_id).all()
+        cart = db.query(cls, Item).filter(cls.item_id==Item.item_id).filter(cls.customer_id==customer_id)\
+            .filter(cls.order_id.is_(None)).all()
         db.close()
         cart_items = []
         if cart:
@@ -152,18 +146,45 @@ class Cart(Base):
             db.refresh(new_entry)
         db.close()
         return True
+    
+    @classmethod
+    def update_cart_items_for_customer(cls, customer_id: int):
+        """Update cart items for the customer_id after checkout"""
+        db = SessionLocal()
+        cart = db.query(cls, Item).filter(cls.item_id==Item.item_id).filter(cls.customer_id==customer_id).all()
+        db.close()
+        cart_items = []
+        if cart:
+            for crt,itm in cart:
+                cart_items.append((itm.title, crt.qty, itm.price, crt.qty*itm.price))
+        return cart_items
+    
+    @classmethod
+    def update_cart_orderId_for_customer(cls, customer_id: int, order_id: int):
+        """Update order_id of cart items for the customer_id after checkout"""
+        db = SessionLocal()
+        cart = db.query(cls).filter(cls.customer_id==customer_id).filter(cls.order_id.is_(None)).all()
+        if cart:
+            for crt_item in cart:
+                print("hi")
+                crt_item.order_id = order_id
+            db.commit()
+        db.close()
+        return True
 
 
 class Order(Base):
     __tablename__ = "orders"
 
     order_id = Column(Integer, primary_key=True, index=True)
-    ordered_items = Column(JSON)
+    order_date = Column(DateTime, default=datetime.utcnow())
+    amount = Column(Float)
     customer_id = Column(Integer, ForeignKey('users.user_id'))
     customer = relationship("User", back_populates="orders")
+    carts = relationship("Cart", back_populates="orders")
 
     def __str__(self):
-        return f"OrderId-{self.order_id}, CustomerId-{self.customer_id}, {self.ordered_items}"
+        return f"OrderId-{self.order_id}, CustomerId-{self.customer_id}"
 
     @classmethod
     def get_order_by_id(cls, order_id: int):
@@ -178,25 +199,31 @@ class Order(Base):
     def get_order_for_customer(cls, customer_id: int):
         """Get ordered items for the customer_id"""
         db = SessionLocal()
-        order = db.query(cls).filter(cls.customer_id==customer_id).first()
+        orders = db.query(cls).filter(cls.customer_id==customer_id).all()
+        orders_dict = {}
+        for order in orders:
+            items_in_order = [
+                {
+                    "title": cart.items.title, 
+                    "qty": cart.qty,
+                    "price": cart.items.price,
+                    "total_price": cart.items.price*cart.qty 
+                } for cart in order.carts
+            ]
+            if items_in_order:
+                orders_dict.update({order.order_id: items_in_order})
         db.close()
-        ordered_items = json.loads(order.ordered_items) if order else {}
-        return ordered_items
+        return orders_dict
     
     @classmethod
     def create_order(cls, order: OrderCreate):
-        """Create or update the order"""
+        """Create order"""
         db = SessionLocal()
-        cust_order = db.query(cls).filter(cls.customer_id==order.customer_id).first()
-        kart_item = json.loads(cust_order.ordered_items) if cust_order else {}
-        kart_item.update(json.loads(order.item_json))
-        if cust_order:
-            cust_order.ordered_items = json.dumps(kart_item)
-            db.commit()
-        else:
-            updated_order = cls(customer_id=order.customer_id, ordered_items=json.dumps(kart_item))
-            db.add(updated_order)
-            db.commit()
-            db.refresh(updated_order)
+        new_order = cls(customer_id=order.customer_id, amount=order.amount)
+        db.add(new_order)
+        db.commit()
+        db.refresh(new_order)
+        new_order_id = new_order.order_id
         db.close()
+        Cart.update_cart_orderId_for_customer(order.customer_id, new_order_id)
         return True
